25070e37f508d8b9e9e0eb2d35ed7458ee443493
fandangos <veggav@gmail.com>
2019-05-29 09:35:17 -0300

    Update WinRenderer.cpp

--- xbmc/cores/VideoPlayer/VideoRenderers/WinRenderer.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/WinRenderer.cpp
@@ -19,6 +19,12 @@
 #include "windows/RendererShaders.h"
 #include "windowing/GraphicContext.h"
 #include "windowing/WinSystem.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nvapi.h"
+#include "uhdDisplay.h"
 
 struct render_details
 {
@@ -38,6 +44,109 @@ struct render_details
   }
 };
 
+if (picture.hasDisplayMetadata || picture.hasLightMetadata)
+void SetHdrMonitorMode(bool enableHDR)
+  {
+      if (first)
+    {
+        NvAPI_Initialize();
+        first = false;
+    }
+
+    NvAPI_Status nvStatus = NVAPI_OK;
+    NvDisplayHandle hNvDisplay = NULL;
+
+    // get first display handle which should work for all NVAPI calls for all GPUs
+    if ((nvStatus = NvAPI_EnumNvidiaDisplayHandle(0, &hNvDisplay)) != NVAPI_OK)
+    {
+        printf("NvAPI_EnumNvidiaDisplayHandle returned error code %d\r\n", nvStatus);
+
+        return;
+    }
+
+    NvU32 gpuCount = 0;
+    NvU32 maxDisplayIndex = 0;
+    NvPhysicalGpuHandle ahGPU[NVAPI_MAX_PHYSICAL_GPUS] = {};
+
+    // get the list of displays connected, populate the dynamic components
+    nvStatus = NvAPI_EnumPhysicalGPUs(ahGPU, &gpuCount);
+
+    if (NVAPI_OK != nvStatus)
+    {
+        printf("NvAPI_EnumPhysicalGPUs returned error code %d\r\n", nvStatus);
+
+        return;
+    }
+
+    for (NvU32 i = 0; i < gpuCount; ++i)
+    {
+        NvU32 displayIdCount = 16;
+        NvU32 flags = 0;
+        NV_GPU_DISPLAYIDS displayIdArray[16] = {};
+        displayIdArray[0].version = NV_GPU_DISPLAYIDS_VER;
+
+        nvStatus = NvAPI_GPU_GetConnectedDisplayIds(ahGPU, displayIdArray, &displayIdCount, flags);
+
+        if (NVAPI_OK == nvStatus)
+        {
+            printf("Display count %d\r\n", displayIdCount);
+
+            for (maxDisplayIndex = 0; maxDisplayIndex < displayIdCount; ++maxDisplayIndex)
+            {
+                printf("Display tested %d\r\n", maxDisplayIndex);
+
+                NV_HDR_CAPABILITIES hdrCapabilities = {};
+
+                hdrCapabilities.version = NV_HDR_CAPABILITIES_VER;
+
+                if (NVAPI_OK == NvAPI_Disp_GetHdrCapabilities(displayIdArray[maxDisplayIndex].displayId, &hdrCapabilities))
+                {
+                    if (hdrCapabilities.isST2084EotfSupported)
+                    {
+                        printf("Display %d supports ST2084 EOTF\r\n", maxDisplayIndex);
+
+                        NV_HDR_COLOR_DATA hdrColorData = {};
+
+                        memset(&hdrColorData, 0, sizeof(hdrColorData));
+
+                        hdrColorData.version = NV_HDR_COLOR_DATA_VER;
+                        hdrColorData.cmd = NV_HDR_CMD_SET;
+                        hdrColorData.static_metadata_descriptor_id = NV_STATIC_METADATA_TYPE_1;
+
+                        hdrColorData.hdrMode = enableHDR ? NV_HDR_MODE_UHDBD : NV_HDR_MODE_OFF;
+
+
+                        nvStatus = NvAPI_Disp_HdrColorControl(displayIdArray[maxDisplayIndex].displayId, &hdrColorData);
+
+                        if (NVAPI_OK == nvStatus)
+                        {
+                            printf("NvAPI_Disp_SethdrColorData call has succeeded: ");
+                        }
+                        else
+                        {
+                            NvAPI_ShortString szDesc;
+                            NvAPI_GetErrorMessage(nvStatus, szDesc);
+                            printf("NvAPI_Disp_HdrColorControl returned %s (%x)\r\n", szDesc, nvStatus);
+                        }
+                    }
+                }
+                else
+                {
+                    NvAPI_ShortString szDesc;
+                    NvAPI_GetErrorMessage(nvStatus, szDesc);
+                    printf("NvAPI_Disp_GetHdrCapabilities returned %s (%x)\r\n", szDesc, nvStatus);
+                }
+            }
+        }
+        else
+        {
+            NvAPI_ShortString szDesc;
+            NvAPI_GetErrorMessage(nvStatus, szDesc);
+            printf("NvAPI_GPU_GetConnectedDisplayIds returned %s (%x)\r\n", szDesc, nvStatus);
+        }
+    }
+  
+  
 static std::vector<render_details> RenderMethodDetails =
 {
   render_details::get<CRendererSoftware>(RENDER_SW, "Software"),
